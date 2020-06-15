841508a2ea79ae29e5fb79ddad59e3b51d3954cd
Marcelo de Moraes <marcelodemoraes11@gmail.com>
2020-06-14 22:45:40 -0300

    background changer

---
+++ change_background.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+
+def left_to_rigth(img, background, bool_img):
+    N, M = bool_img.shape
+
+    for x in range(0, N):
+        y = 0
+        while y < N and bool_img[x][y] == False:
+            y += 1
+        for y in range(y, M):
+            if bool_img[x][y] == False:
+                break
+            else:
+                img[x][y] = background[x][y]
+
+    return img
+
+
+def right_to_left(img, background, bool_img):
+    N, M = bool_img.shape
+
+    for x in range(N - 1, 0, -1):
+        y = M - 1
+        while y >= 0 and bool_img[x][y] == False:
+            y -= 1
+        for y in range(y, 0, -1):
+            if bool_img[x][y] == False:
+                break
+            else:
+                img[x][y] = background[x][y]
+
+    return img
+
+
+def change_background(img, background, bool_img):
+
+    img = left_to_rigth(img, background, bool_img)
+    img = right_to_left(img, background, bool_img)
+
+    return img
