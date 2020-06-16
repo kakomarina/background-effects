737dc80ccc6c4f049d6cd4d63c2afb894b83c4a3
Marina Kako <ninakako@hotmail.com>
2020-06-15 21:07:24 -0300

    added comments and new images

--- triangle_threshold.py
+++ triangle_threshold.py
@@ -1,7 +1,9 @@
 import numpy as np
 import math
 
-
+# using the equation of a straight line y = mx + b
+# to calculate the points in the line between the peak
+# and the end of the histogram
 def get_y(xi, peak, end, hist):
     m = (hist[end] - hist[peak]) / (end - peak)
     b = hist[end] - m * end
@@ -13,7 +15,7 @@ def get_x(yi, peak, end, hist):
     m = (hist[end] - hist[peak]) / (end - peak)
     b = hist[end] - m * end
 
-    return (yi - b)/m
+    return (yi - b) / m
 
 
 def histogram(img, no_levels=255):
@@ -39,39 +41,44 @@ def median_filter(img, k=5):
     r = np.zeros(img.shape)
     for x in np.arange(a, img.shape[0] - a + 1):
         for y in np.arange(a, img.shape[1] - a + 1):
-            med_region = np.median(img[x - a: x + a + 1, y - a: y + a + 1])
+            med_region = np.median(img[x - a : x + a + 1, y - a : y + a + 1])
             r[x, y] = med_region
 
     return r
 
 
-def triangule_threshold(img):
+def triangle_threshold(img):
+    # calculating image histogram
     hist = histogram(img)
-    peak = np.argmax(hist)
+    # finding the intensity with more occurrences in the histogram
+    peak = np.argmax(hist[1:]) + 1
 
     if 255 - peak > peak:
-        # peak está mais perto do começo
+        # peak is closer to the beggining, so triangle must face the end
         end = 254
         index = np.arange(peak + 1, end)
     else:
-        # peak está mais perto do final
+        # peak is closer to the end, so triangle must face the beggining
         end = 0
         index = np.arange(0, peak)
 
     d_max = -1
     split = 0
 
+    # for every triangle between the peak and the farthest intensity value in the histogram
+    # calculate it's height and choose the value with the biggest height as threshold
     for i in index:
         y1 = abs(get_y(i, peak, end, hist))
         x1 = abs(get_x(hist[i], peak, end, hist))
 
         a = y1 - hist[i]
         b = x1 - i
-        c = float(math.sqrt(a**2 + b**2))
-        d = (float(b)*float(a))/c
+        c = float(math.sqrt(a ** 2 + b ** 2))
+        d = (float(b) * float(a)) / c
 
         if d > d_max:
-            d_max = a
+            d_max = d
             split = i
 
-    return (img) > split
+    # return image with the threshold applied
+    return split
