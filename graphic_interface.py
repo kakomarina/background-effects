f477003e8b39d19dff96dac062433f2c1f535566
Beatriz Monteiro <monteiro.beatriz70@gmail.com>
2020-06-26 18:07:50 -0300

    gets pixel of clicked color and saves it in an array

--- graphic_interface.py
+++ graphic_interface.py
@@ -11,17 +11,26 @@ from PIL import ImageTk,Image
 import numpy as np
 import imageio
 
+
 def motion(event):
     x, y = event.x, event.y
-    if(onClick):
-        print('clicado {}, {}'.format(x, y))
-    else:
-        print('solto {}, {}'.format(x, y))
-    
+  #  if(onClick):
+    print('{}, {}'.format(x, y))
+ #   else:
+ #       print('solto {}, {}'.format(x, y))
+
 def callback(event):
-    global onClick
-    onClick = True
+#    global onClick
+#    onClick = True
     print("clicked at", event.x, event.y)
+    global x, y
+    x = event.x
+    y = event.y
+    im = Image.open("girl1.jpg")
+    pixel = im.load()
+    colors.append(pixel[x,y])
+    print(colors)
+    
 
 def onClickFalse(event):
     global onClick
@@ -34,16 +43,14 @@ img = np.array(input_img)
 img = img.astype(np.int32) #casting para realizar as funcoes
 #creates mold for final image
 altura, largura, profundidade = img.shape
-
-onClick = False
+colors = []
+#onClick = False
 root = tkr.Tk()
 canvas = tkr.Canvas(root, width=largura, height=altura)
 canvas.grid()
 img = ImageTk.PhotoImage(Image.open("girl1.jpg"))  
-canvas.create_image(0, 0, anchor=NW, image=img) 
+canvas.create_image(0, 0, anchor="nw", image=img) 
 root.bind('<Motion>', motion)
 root.bind('<Button-1>', callback)
-root.bind('<ButtonRelease-1>', onClickFalse)
-
 
 root.mainloop()
